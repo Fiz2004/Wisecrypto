8706eb5c8a62a9b5f1f0710cde109c0da83c0f6f
Fiz2004 <Fiz2004@Rambler.ru>
2022-06-17 09:49:26 +0700

    add colors

--- app/src/main/java/com/fiz/wisecrypto/ui/theme/Color.kt
+++ app/src/main/java/com/fiz/wisecrypto/ui/theme/Color.kt
@@ -2,6 +2,18 @@ package com.fiz.wisecrypto.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
+val LightGreen = Color(0xFF_5E_DE_99)
+val Pink = Color(0xFF_F2_88_F4)
+val DarkGreen = Color(0xFF_00_CB_6A)
+val Warning = Color(0xFF_EF_BE_24)
+val Red = Color(0xFF_F2_66_66)
+val Dark = Color(0xFF_04_15_0C)
+val Light = Color(0xFF_F7_F7_F7)
+val Black = Color(0xFF_15_2C_07)
+val Gray = Color(0xFF_5D_5C_5D)
+val Gray2 = Color(0xFF_BE_BE_BE)
+val White = Color(0xFF_FF_FF_FF)
+
 val Purple80 = Color(0xFFD0BCFF)
 val PurpleGrey80 = Color(0xFFCCC2DC)
 val Pink80 = Color(0xFFEFB8C8)
