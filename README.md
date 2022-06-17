af519987bbdea556cfe50ebb7fb508279658cfc8
Fiz2004 <Fiz2004@Rambler.ru>
2022-06-17 09:12:10 +0700

    init

---
+++ README.md
@@ -0,0 +1,4 @@
+# Wisecrypto
+
+Track your favorite stocks on the go and stay informed with personalized alerts and global financial
+news.
---
+++ app/src/main/java/com/fiz/wisecrypto/MainActivity.kt
@@ -0,0 +1,43 @@
+package com.fiz.wisecrypto
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.fiz.wisecrypto.ui.theme.WisecryptoTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            WisecryptoTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    Greeting("Android")
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String) {
+    Text(text = "Hello $name!")
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    WisecryptoTheme {
+        Greeting("Android")
+    }
+}
